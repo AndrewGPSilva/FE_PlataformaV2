376c7bb40709056be3548e385fc99c39e1abecb6
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-13 22:09:50 -0300

    first commit

---
+++ pages/aulas.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h1>Aulas:</h1>
+        <ul>
+            <li v-for="aula in dados" :key="aula.id">
+                <h2>{{ aula.titulo }}</h2>
+                <p>{{ aula.descricao }}</p>
+                <p>Duração: {{ aula.duracao }} minutos</p>
+                <p>Status: {{ aula.status }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+interface Aula {
+    id: number;
+    titulo: string;
+    descricao: string;
+    duracao: number;
+    status: boolean;
+}
+
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            dados: {} as Record<string, Aula[]>,
+        };
+    },
+    methods: {
+        aulas() {
+            const token = localStorage.getItem("Token");
+            const headers = {
+                'Authorization': `Bearer ${token}`,
+            };
+            axios.get("http://localhost:8080/api/dados/aulas", { headers })
+                .then((response) => {
+                    if (response.status === 200) {
+                        this.dados = response.data['Aulas Encontradas: '];
+                    } else {
+                        console.error('Erro na requisição:', response.status);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Erro ao obter dados:', error);
+                });
+        },
+    },
+    created() {
+        this.aulas();
+    },
+};
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,45 @@
+<template>
+    <main>
+        <h1>Login</h1>
+        <form @submit.prevent="efetuarLogin">
+            <label for="login">login</label>
+            <input type="text" name="login" id="login" v-model="login">
+            <label for="senha">senha</label>
+            <input type="password" name="senha" id="senha" v-model="senha">
+            <button type="submit">Login</button>
+        </form>
+    </main>
+</template>
+
+<script lang="ts">
+import axios from "axios"
+
+export default {
+    data() {
+        return {
+            login: "",
+            senha: ""
+        }
+    },
+    methods: {
+        efetuarLogin() {
+            const dados = {
+                login: this.login,
+                senha: this.senha
+            }
+
+            axios.post("http://localhost:8080/api/login", dados)
+                .then((response) => {
+                    const Token = response.data.token;
+
+                    localStorage.setItem('Token', Token);
+
+                    this.$router.push({ path: "/aulas"});
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
+    }
+}
+</script>
