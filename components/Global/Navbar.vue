d80ee5c80a8b1bc1a3594c2a85c8257438717072
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-18 13:45:54 -0300

    feat: Navbar concluida

--- components/Global/Navbar.vue
+++ components/Global/Navbar.vue
@@ -1,10 +1,70 @@
 <template>
     <nav>
+        <div class="container-titulo">
+            <h1>
+                <NuxtLink to="/">TI-FREE</NuxtLink>
+            </h1>
+        </div>
+        <div class="container-links">
+            <NuxtLink to="/roadmap">
+                Roadmap
+            </NuxtLink>
+            <NuxtLink to="/cursos">
+                Cursos
+            </NuxtLink>
+            <NuxtLink to="/login">
+                Entrar
+            </NuxtLink>
+            <button>
+                Matricule-Se
+            </button>
+        </div>
     </nav>
 </template>
 
 <style scoped>
-    nav {
-        background-color: #01080E;
-    }
+nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 30px;
+    background-color: #01080E;
+    color: white;
+    align-items: center;
+}
+
+.container-titulo {
+    font-family: "Contrail One", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 25px;
+    cursor: pointer;
+}
+
+.container-titulo a {
+    color: white;
+}
+
+.container-links {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+}
+
+.container-links a {
+    color: white;
+    padding: 10px;
+}
+
+.container-links a:hover {
+    background-color: gray;
+    border-radius: 10px;
+}
+
+.container-links button {
+    background-color: aqua;
+    padding: 15px;
+    color: black;
+    font-weight: bold;
+    cursor: pointer;
+}
 </style>
